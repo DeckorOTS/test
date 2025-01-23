402d666850fded9fc1e312d3750210341d01f79e
DeckorOTS <53954215+DeckorOTS@users.noreply.github.com>
2025-01-22 23:43:45 -0800

    Delete todo-app directory

--- todo-app/src/components/UI/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = ({ text, onClick, type = 'button' }) => (
-    <button type={type} onClick={onClick} className='btn'>
-        {text}
-    </button>
-);
-
-export default Button;
