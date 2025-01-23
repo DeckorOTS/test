58ff891dcd5b046d62c2e35aaec58a2b35e5363b
DeckorOTS <53954215+DeckorOTS@users.noreply.github.com>
2025-01-23 13:21:12 -0800

    src/pages/ChatbotEditorPage.js

---
+++ src/pages/ChatbotEditorPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatbotEditor from '../components/ChatbotEditor';
+
+const ChatbotEditorPage = () => {
+    return (
+        <div>
+            <h1>Editor de Chatbot</h1>
+            <ChatbotEditor />
+        </div>
+    );
+};
+
+export default ChatbotEditorPage;
