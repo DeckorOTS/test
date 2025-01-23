44673140653fefe1ee696262d9ca89484d4b530a
DeckorOTS <53954215+DeckorOTS@users.noreply.github.com>
2025-01-22 22:43:57 -0800

    Componente para mostrar modales que pueden abrirse y cerrarse. Maneja la superposición de fondo y permite la inclusión de contenido personalizado dentro del modal.

---
+++ src/components/Modal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Modal.css'; // Asumiendo que existen estilos en este archivo
+
+const Modal = ({ isOpen, onClose, children }) => {
+    if (!isOpen) return null;
+
+    return (
+        <div className="modal-overlay">
+            <div className="modal-content">
+                <button className="close-button" onClick={onClose}>&times;</button>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+Modal.propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired,
+    children: PropTypes.node.isRequired,
+};
+
+export default Modal;
